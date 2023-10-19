e6234ed68389a050fe60eb8758c57b83da626230
Jygo547 <jygo3715@colorado.edu>
2023-10-19 11:08:58 -0600

    Secondary Commit

--- Oct 19th App Design/ContentView.swift
+++ Oct 19th App Design/ContentView.swift
@@ -12,8 +12,13 @@ struct ContentView: View {
     @State private var isLiked: Bool = UserDefaults.standard.bool(forKey: "isLiked")
     @State private var cardExpanded = false
     
-    private var tabWidth: CGFloat = 100
+    func toggleLike() {
+        isLiked.toggle()
+        UserDefaults.standard.set(isLiked, forKey: "isLiked")
+    }
     
+    private var tabWidth: CGFloat = 100
+
     var body: some View {
         VStack {
             VStack(alignment: .center) {
@@ -25,27 +30,35 @@ struct ContentView: View {
                     
                     Spacer()
                     
-                    ForEach(["Stay Local", "Go Global"], id: \.self) {
-                        tab in
-                        Text(tab)
-                            .padding()
-                            .font(.caption)
-                            .background(selectedTab == tab ? Color.red : Color.gray.opacity(0.2))
-                            .onTapGesture {
-                                withAnimation {
-                                    self.selectedTab = tab
-                            
-                                }
-                            }
-                        if tab == "Stay Local" {
-                            Spacer()
-                                .frame(width: 35)
-                        }
+                    HStack{
+                        Spacer()
+                            .frame(width: 10)
                         
+                        ForEach(["Stay Local", "Go Global"], id: \.self) {
+                            tab in
+                            Text(tab)
+                                .padding()
+                                .font(.caption)
+                                .background(selectedTab == tab ? Color.red : Color.red.opacity(0))
+                                .cornerRadius(5)
+                                .onTapGesture {
+                                    withAnimation {
+                                        self.selectedTab = tab
+                                
+                                    }
+                                }
                             
+                                
+                        }
+                        .padding(.vertical)
+                        
+                        Spacer()
+                            .frame(width: 10)
+                        
                     }
-                    
-                    .padding(.vertical)
+                    .background(Color.blue)
+                    .frame(height: 70)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
                     
                     
                     Spacer()
@@ -56,81 +69,96 @@ struct ContentView: View {
                         .frame(width: 40, height: 40)
                         .clipShape(Circle())
                 }
+                .padding(.horizontal, 20)
                 
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        Text("Rec")
-                            .padding()
-                            .background(Color.red)
-                            .cornerRadius(15)
-                        Text("Experience")
-                            .padding()
-                            .background(Color.red)
-                            .cornerRadius(15)
-                        Text("Class")
-                            .padding()
-                            .background(Color.red)
-                            .cornerRadius(15)
-                    }
-                }
-                .padding(.horizontal)
-                
-                VStack(alignment: .leading) {
-                    HStack {
-                        Image("ProfilePicture")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 30, height: 30)
-                            .clipShape(Circle())
-                            .padding(.leading, -5)
-                        Text("Jane Doe")
-                            .bold()
-                            .padding(.leading, 5)
+                ScrollView(.vertical, showsIndicators: true) {
+                    
+                        VStack {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack {
+                                    Text("Rec")
+                                        .padding()
+                                        .background(Color.red)
+                                        .cornerRadius(15)
+                                    Text("Experience")
+                                        .padding()
+                                        .background(Color.red)
+                                        .cornerRadius(15)
+                                    Text("Class")
+                                        .padding()
+                                        .background(Color.red)
+                                        .cornerRadius(15)
+                                }
+                            }
                             
-                        Spacer()
-                            .frame(width: 100)
-                        
-                        Button(action: {
-                            isLiked.toggle()
-                            UserDefaults.standard.set(isLiked, forKey: "isLiked")
-                        }) {
-                            Image(systemName: isLiked ? "heart.fill" : "heart")
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Image("ProfilePicture")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 30, height: 30)
+                                        .clipShape(Circle())
+                                        .padding(.leading, -5)
+                                    Text("Jane Doe")
+                                        .bold()
+                                        .padding(.leading, 5)
+                                        
+                                    Spacer()
+                                        .frame(width: 95)
+                                    
+                                    Button(action: toggleLike) {
+                                        Image(systemName: isLiked ? "heart.fill" : "heart")
+                                            
+                                            .foregroundColor(isLiked ? .red : .black)
+                                    }
+                                    
+                                    Image(systemName: "bookmark")
+                                    Image(systemName: "message.badge")
+                                    Text("2")
+                                        .font(.callout)
+                                        .padding(.leading, -5)
+                                        .padding(.bottom, -25)
+                                }
+                                .padding()
+                                
+                                Text("12 October 2023")
+                                    .font(.footnote)
+                                    .padding(.horizontal)
+                                Text("La Cuisine Paris: Dessert cooking class")
+                                    .font(.headline)
+                                    .padding(.horizontal)
+                                    .padding(.vertical, 5)
+                                Text("Learn to make your own croissants!! The centrally located La Cuisine Paris cooking school has a range of culinary courses in English...")
+                                    .font(.callout)
+                                    .lineLimit(cardExpanded ? nil : 3)
+                                    .padding()
+                                    .padding(.top, -15)
+                            }
+                            .background(Color.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                            .frame(height: 200)
+                            .onTapGesture {
+                                withAnimation {
+                                    cardExpanded.toggle()
+                                }
+                            }
+                            
                         }
-                        Image(systemName: "bookmark")
-                        Image(systemName: "message.badge")
-                        Text("2")
-                            .font(.callout)
-                            .padding(.leading, -5)
-                            .padding(.bottom, -25)
-                    }
-
-                    
-                    .padding()
-                    
-                    Text("12 October 2023")
-                        .font(.footnote)
                         .padding(.horizontal)
-                    Text("La Cuisine Paris: Dessert cooking class")
-                        .font(.headline)
-                        .padding(.horizontal)
-                        .padding(.vertical, 5)
-                    Text("Learn to make your own croissants!! The centrally located La Cuisine Paris cooking school has a range of culinary courses in English...")
-                        .font(.callout)
-                        .lineLimit(cardExpanded ? nil : 3)
-                        .padding()
-                        .padding(.top, -15)
-                }
-                .background(Color.blue)
-                .onTapGesture {
-                    withAnimation {
-                        cardExpanded.toggle()
-                    }
+                        .frame(height: 600)
+                        .background(
+                            Image("ProfilePicture")
+                                .resizable()
+                                .scaledToFit()
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                
+                        )
+                        .shadow(radius: 10)
+                        .padding(.bottom, 15)
+                        .padding(.horizontal, 40)
+                    
                 }
             }
-            .padding(.horizontal)
-            .background(Color.white)
-            .cornerRadius(15)
-            .shadow(radius: 5)
             
             Spacer()
             
@@ -148,6 +176,10 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    return ContentView()
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
